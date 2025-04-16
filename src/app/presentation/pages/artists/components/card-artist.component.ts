25d9812ec8f89bc3e0b2a6884ca8db9c74bf0e2c
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-04-16 15:59:22 -0500

    feat: mejorar diseño de componentes de artistas; ajustar clases de estilo y corregir atributos de imagen

--- src/app/presentation/pages/artists/components/card-artist.component.ts
+++ src/app/presentation/pages/artists/components/card-artist.component.ts
@@ -4,12 +4,12 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'card-artist',
   template: `
     <div
-      class="flex flex-col items-center p-6 overflow-hidden text-center transition shadow-xl bg-zinc-900 rounded-2xl hover:shadow-2xl"
+      class="w-full h-full flex flex-col items-center p-6 overflow-hidden text-center transition shadow-xl bg-zinc-900 rounded-2xl hover:shadow-2xl"
     >
       <img
         [src]="fotoUrl"
-        alt="Artista"
-        class="object-cover mb-4 border-4 border-pink-500 rounded-full w-28 h-28"
+        [alt]="name"
+        class="w-28 h-28 object-cover mb-4 border-4 border-pink-500 rounded-full block"
       />
       <h3 class="text-lg font-bold">{{ name }}</h3>
       <p class="mb-2 text-sm text-gray-400">Cantante / Actriz</p>
