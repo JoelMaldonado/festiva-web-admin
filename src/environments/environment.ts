f7b938b0c9082935e98b06bbdab4d856e45fb131
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-06-27 11:19:17 -0500

    feat: actualizar baseUrl en entornos de desarrollo a la URL correcta de la API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://atmosfera-soltec.com:3245/api',
+  baseUrl: 'https://api.festiva.no:3245/api',
   mapboxToken:
     'pk.eyJ1IjoiamptZmRldiIsImEiOiJjbTgybXg0YjUxbGs1MmpxM210OTdmbTE2In0.AjmCpDHaiKjNj-XosGXXuA',
   firebaseConfig: {
