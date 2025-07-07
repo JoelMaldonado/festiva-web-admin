cba2564d049deff62a9c6054fd451f933dc68d7e
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-07-07 17:20:30 -0500

    feat: eliminar componente de ubicación de clubes y limpiar código relacionado

--- src/app/presentation/features/clubs/pages/home/list-clubs.component.ts
+++ src/app/presentation/features/clubs/pages/home/list-clubs.component.ts
@@ -1,26 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { delay } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { AddClubComponent } from './components/add-club/add-club.component';
-import { ClubLocationComponent } from './components/club-location/club-location.component';
 import { ClubService } from '@services/club.service';
 import { Club } from '@dto/club';
-import { BtnUploadImageComponent } from '../../../../components/btn-upload-image.component';
 import { FileUploadModule } from 'primeng/fileupload';
 import { DrawerModule } from 'primeng/drawer';
 import { FloatLabelModule } from 'primeng/floatlabel';
-import {
-  ImageFirebase,
-  UploadImageUseCase,
-} from 'app/domain/usecase/upload-image.usecase';
-import { InputComponent } from '../../../../components/input/input.component';
 
 @Component({
   selector: 'app-list-clubs',
@@ -31,20 +23,16 @@ import { InputComponent } from '../../../../components/input/input.component';
     TableModule,
     ButtonModule,
     TooltipModule,
-    ClubLocationComponent,
     DialogModule,
     AddClubComponent,
     InputTextModule,
     FileUploadModule,
     DrawerModule,
     FloatLabelModule,
-    InputComponent,
   ],
   templateUrl: './list-clubs.component.html',
 })
 export class ListClubsComponent implements OnInit {
-  private readonly uploadImageUseCase = inject(UploadImageUseCase);
-
   clubs: Club[] = [];
   clubsFiltered: Club[] = [];
 
@@ -69,8 +57,6 @@ export class ListClubsComponent implements OnInit {
   }
 
   value: string | undefined;
-  visible = false;
-  selectedClubId: number | null = null;
   isLoading = false;
   router = inject(Router);
 
@@ -107,43 +93,11 @@ export class ListClubsComponent implements OnInit {
     this.router.navigate(['menu', 'clubs', id]);
   }
 
-  openDrawer(id: number) {
-    this.selectedClubId = id;
-    this.visible = true;
-  }
-
   dialogEditLogo = false;
 
   openEditLogo() {
     this.dialogEditLogo = true;
   }
 
-  findPhones(id: number) {
-    this.clubService.fetchClubPhones(id).subscribe({
-      next: (res) => {
-        if (res.isSuccess) {
-          console.log(res.data);
-        } else {
-          console.error(res.message);
-        }
-      },
-    });
-  }
-
   clubSelected?: Club;
-  nameClub = new FormControl('');
-  descriptionClub = new FormControl('');
-  clubImageFile: File | null = null;
-
-  myValor = '';
-  myValorError?: string;
-
-  newValor(event: any) {
-  
-    this.myValor = event;
-    this.myValorError =
-      this.myValor.length < 3
-        ? 'El valor debe tener al menos 3 caracteres.'
-        : undefined;
-  }
 }
