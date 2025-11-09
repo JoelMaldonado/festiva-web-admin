3b187bb0cb799c1ba75c722a8ecaa4ff3bf23cf2
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-11-09 01:33:39 -0500

    feat: actualizar la URL base de la API en el entorno de configuración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://api.festiva.no:8001/api',
+  baseUrl: 'https://api.festiva.no/api',
   mapboxToken:
     'pk.eyJ1IjoiamptZmRldiIsImEiOiJjbTgybXg0YjUxbGs1MmpxM210OTdmbTE2In0.AjmCpDHaiKjNj-XosGXXuA',
   firebaseConfig: {
