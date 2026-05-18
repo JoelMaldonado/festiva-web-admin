ea3b5528ad091136cc30e2207f62bb42bb0407d8
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-05-18 15:50:42 -0500

    feat: mejorar diseño del panel de login y optimizar el código HTML

--- src/app/presentation/features/login/login.component.ts
+++ src/app/presentation/features/login/login.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -7,7 +6,7 @@ import { AuthService } from 'app/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
