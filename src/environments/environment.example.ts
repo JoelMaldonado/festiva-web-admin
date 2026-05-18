6c21f84a61a45ea5f695001762d78e96701ccc4a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2026-05-18 15:01:41 -0500

    feat: agregar archivo de configuración de entorno y actualizar flujo de despliegue

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  baseUrl: '',
+  mapboxToken: '',
+  firebaseConfig: {
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
+  },
+  production: true,
+  version: '1.0.1',
+};
